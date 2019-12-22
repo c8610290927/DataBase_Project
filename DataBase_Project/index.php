2e1cfdd67db76a1caee46dc17cc4a325cca95363
yanqing0709 <smile870709@yahoo.com.tw>
2019-12-22 20:28:06 +0800

    add index.html&index.php

---
+++ DataBase_Project/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	  <title>海大班級訂書系統-首頁</title>
+	  
+	  <!-- bootstrap的引入條件-->
+	  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+	  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+	</head>
+
+    <body>
+	    <?php
+            include "db_finalproject_conn.php";
+		?>
+	  	<nav class="navbar navbar-expand-xl navbar-dark bg-dark" id="mainNav">
+			<div class="container">
+				  <a class="navbar-brand" href="index.html">海大班級訂書系統</a>
+				  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				  </button>
+				
+				<div class="navbar-collapse collapse" id="navbarText">
+				  <ul class="nav navbar-nav">
+					<li class="nav-item"><a class = "nav-link" href="member_information.html">會員資訊</a></li>
+					<li class="nav-item"><a class = "nav-link" href="book_information.html">訂購書籍資訊</a></li>          
+                    
+					<li class="dropdown">
+						<a class="nav-link dropdown-toggle active manager_name" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<img src="https://fashion.jedi.net.tw/images/user.png" width=30 height=30>
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						  <a class="dropdown-item" href="login.html">登入</a>
+						</div>
+					</li>
+				  </ul>
+				</div>
+			</div>
+	    </nav>
+		<p align="center"><center>歡迎來到此系統，請由下拉式選單並於登入後進行操作</center></p>
+	</body>
+</html>
